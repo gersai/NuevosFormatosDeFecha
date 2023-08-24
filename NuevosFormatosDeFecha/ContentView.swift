cb14c17def52dd23aa783b9aca7245ee97869dc2
Gersai <gersai_ventura@hotmail.com>
2023-08-24 13:48:24 -0600

    Initial Commit

---
+++ NuevosFormatosDeFecha/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NuevosFormatosDeFecha
+//
+//  Created by Eduardo Gersai Garcia Ventura on 24/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NuevosFormatosDeFecha/NuevosFormatosDeFechaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NuevosFormatosDeFechaApp.swift
+//  NuevosFormatosDeFecha
+//
+//  Created by Eduardo Gersai Garcia Ventura on 24/08/23.
+//
+
+import SwiftUI
+
+@main
+struct NuevosFormatosDeFechaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
